f4543ece0b1194de44804406929c26997b3312be
Sachin Gupta <sachin070gupta@gmail.com>
2021-10-12 13:43:35 +0530

    Module 3 Completed

--- src/main/java/com/pluralsight/blog/BlogApplication.java
+++ src/main/java/com/pluralsight/blog/BlogApplication.java
@@ -8,6 +8,8 @@ public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
+		
+		System.out.println("Server running on port 8083...");
 	}
 
 }
--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,32 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
 @Controller
 public class BlogController {
 
+	private PostRepository postRepository;
+	
+	
+	
+	public BlogController(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
+
+
+
 	@RequestMapping("/")
 	public String listPosts(ModelMap mMap) {
-		mMap.put("title", "Blog Post 1");
+		List<Post> posts1 = postRepository.getAllPosts();
+		mMap.put("posts", posts1);
+		mMap.put("title", posts1);
 		return "home";
 	}
 }
